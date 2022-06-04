2af8508da5a2e301dec3f836cc6236118a1ff2ce
Burt <2902978956@qq.com>
2022-06-04 21:18:28 +0800

    顶部 tab 的实现

--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, StatusBar, Dimensions} from 'react-native';
+import {TabView, SceneMap} from 'react-native-tab-view';
+
+const FirstRoute = () => (
+  <View style={[styles.scene, {backgroundColor: '#ff4081'}]} />
+);
+
+const SecondRoute = () => (
+  <View style={[styles.scene, {backgroundColor: '#673ab7'}]} />
+);
+
+const initialLayout = {width: Dimensions.get('window').width};
+const renderScene = SceneMap({
+  tab1: FirstRoute,
+  tab2: SecondRoute,
+});
 
 const PopularPage = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {key: 'tab1', title: 'Tab1'},
+    {key: 'tab2', title: 'Tab2'},
+  ]);
   return (
-    <View style={styles.container}>
-      <Text>popular Page</Text>
-    </View>
+    <TabView
+      onIndexChange={setIndex}
+      navigationState={{index, routes}}
+      renderScene={renderScene}
+      initialLayout={initialLayout}
+      style={styles.container}
+    />
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: StatusBar.currentHeight,
+  },
+  scene: {
     flex: 1,
-    backgroundColor: '#ccc',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
