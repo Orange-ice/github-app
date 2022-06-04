3417fca1e7aa96dd1883b30772a0eb96f0af0250
Burt <2902978956@qq.com>
2022-06-04 19:23:54 +0800

    引入 react-native-vector-icons 添加底部导航 icon

--- App.js
+++ App.js
@@ -8,30 +8,56 @@ import MyPage from './js/page/MyPage';
 import PopularPage from './js/page/PopularPage';
 import TrendingPage from './js/page/TrendingPage';
 import FavoritePage from './js/page/FavoritePage';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{headerShown: false}}>
       {/*<Tab.Screen name="Home" component={HomePage} />*/}
       <Tab.Screen
         name="Popular"
         component={PopularPage}
-        options={{title: '最热'}}
+        options={{
+          title: '最热',
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="whatshot" size={26} style={{color}} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Trending"
         component={TrendingPage}
-        options={{title: '趋势'}}
+        options={{
+          title: '趋势',
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="trending-up" size={26} style={{color}} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Favorite"
         component={FavoritePage}
-        options={{title: '收藏'}}
+        options={{
+          title: '收藏',
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="favorite" size={26} style={{color}} />
+          ),
+        }}
       />
-      <Tab.Screen name="My" component={MyPage} options={{title: '我的'}} />
+      <Tab.Screen
+        name="My"
+        component={MyPage}
+        options={{
+          title: '我的',
+          tabBarIcon: ({color}) => (
+            <Entypo name="user" size={26} style={{color}} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
